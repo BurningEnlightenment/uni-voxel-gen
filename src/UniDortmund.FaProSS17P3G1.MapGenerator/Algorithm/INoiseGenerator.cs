adb5d273a1137f5746d3c4189f0cd6ac90f6a3e4
Henrik S. Gaßmann <henrik@gassmann.onl>
2017-07-03 17:44:00 +0200

    ADD SIMPLEX NOISE GENERATOR INTERFACE

---
+++ src/UniDortmund.FaProSS17P3G1.MapGenerator/Algorithm/INoiseGenerator.cs
@@ -0,0 +1,8 @@
+﻿namespace UniDortmund.FaProSS17P3G1.MapGenerator.Algorithm
+{
+    public interface INoiseGenerator
+    {
+        float this[float x, float y] { get; }
+        float this[float x, float y, float z] { get; }
+    }
+}
---
+++ src/UniDortmund.FaProSS17P3G1.MapGenerator/Algorithm/SimplexNoiseGenerator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UniDortmund.FaProSS17P3G1.MapGenerator.Algorithm
+{
+    public class SimplexNoiseGenerator : INoiseGenerator
+    {
+        private readonly FastNoise mImpl;
+
+        public SimplexNoiseGenerator(ulong seed)
+        {
+            mImpl = new FastNoise((int)seed);
+        }
+
+        float INoiseGenerator.this[float x, float y]
+            => mImpl.GetSimplex(x, y);
+
+        float INoiseGenerator.this[float x, float y, float z]
+            => mImpl.GetSimplex(x, y, z);
+    }
+}
