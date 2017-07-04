bc4a71c363b2553b1f8794a94ea38368a0ac0e34
Henrik S. Gaßmann <Henrik.Gassmann@adesso.de>
2017-07-04 15:17:15 +0200

    ADD UNPACKED COlUMN SKELETON

---
+++ src/UniDortmund.FaProSS17P3G1.MapGenerator/Model/UnpackedColumn.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UniDortmund.FaProSS17P3G1.MapGenerator.Model
+{
+    public class UnpackedChunk
+    {
+        private ParticleType[,,] mparticles;
+
+        public ParticleType this[int x, int y, int z]
+        {
+            get => throw new NotImplementedException();
+            set => throw new NotImplementedException();
+        }
+    }
+
+    public class UnpackedColumn
+    {
+        private BiomeType[,] mbiomeTypes;
+        private List<UnpackedChunk> mChunks;
+
+        public BiomeType this[int x, int y]
+        {
+            get => throw new NotImplementedException();
+            set => throw new NotImplementedException();
+        }
+
+        public ParticleType this[int x, int y, int z]
+        {
+            get => throw new NotImplementedException();
+            set => throw new NotImplementedException();
+        }
+    }
+}
