7a8a095db9f4f28f0b2090b46da04373344276a0
Henrik S. Gaßmann <henrik@gassmann.onl>
2017-07-04 09:39:48 +0200

    ADD MAP GENERATOR UNIT TEST PROJECT

---
+++ src/UniDortmund.FaProSS17P3G1.MapGenerator.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace UniDortmund.FaProSS17P3G1.MapGenerator.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
