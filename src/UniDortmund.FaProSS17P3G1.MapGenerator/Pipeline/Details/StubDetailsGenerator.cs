91bc04edf29bce002655a14b1da8a05c36802c69
Henrik S. Gaßmann <henrik@gassmann.onl>
2017-07-09 10:40:54 +0200

    IMPLEMENT STUB DETAILS GENERATOR

--- src/UniDortmund.FaProSS17P3G1.MapGenerator/Pipeline/Details/StubDetailsGenerator.cs
+++ src/UniDortmund.FaProSS17P3G1.MapGenerator/Pipeline/Details/StubDetailsGenerator.cs
@@ -7,13 +7,12 @@ namespace UniDortmund.FaProSS17P3G1.MapGenerator.Pipeline.Details
 {
     public class StubDetailsGenerator : IDetailsGenerator
     {
-        public INoiseGenerator NoiseGenerator { get; set; }
+        public INoiseGenerator NoiseGenerator { set { } }
 
         public static StubDetailsGenerator Create(DetailsGeneratorSettings _) => new StubDetailsGenerator();
 
         public void ApplyTo(WorldMap targetMap, int x, int y)
         {
-            throw new NotImplementedException();
         }
     }
 }
